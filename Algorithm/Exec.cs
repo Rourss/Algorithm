8e790ccbf936ae8d14934f82013288bb88c65ba5
ROURSS <47970410+Rourss@users.noreply.github.com>
2019-12-12 17:02:25 +0800

    创建练习类，实现10000以内的完美数算法

---
+++ Algorithm/Exec.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+
+namespace Algorithm
+{
+    public class Exec
+    {
+        /// <summary>
+        /// 完美数的算法
+        /// </summary>
+        /// <returns>The perfect number.</returns>
+        /// <param name="end">End.</param>
+        public ArrayList FindPerfectNum(int end)
+        {
+            ArrayList arr = new ArrayList();
+            int sum = 0;
+            DateTime startime = DateTime.Now;
+            for (int i = 1; i < end; i++)
+            {
+                for (int j = 1; j < i / 2 + 1; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        sum += j;
+                    }
+                }
+                if (sum == i)
+                {
+                    arr.Add(sum);
+                }
+                sum = 0;
+            }
+            DateTime endtime = DateTime.Now;
+            arr.Add((endtime - startime).TotalSeconds);
+            return arr;
+        }
+    }
+}
--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -6,7 +6,21 @@ namespace Algorithm
     {
         static void Main(string[] args)
         {
-            
+            var ex =new Exec();
+            #region
+            var num = ex.FindPerfectNum(10000);
+            if (num != null)
+            {
+                foreach (var item in num)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Not PerfectNumber");
+            }
+            #endregion
         }
     }
 }
